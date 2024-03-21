add139ec0a1583a50d674f909e99e0e8c51f544b
miguel5407 <miguelantonioteixeirarosa@gmail.com>
2024-03-21 18:53:08 -0300

    adicionei o exercicio 1

---
+++ Exercicio1.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Program{
+    static void Main(){
+        Console.WriteLine("digite um numero inteiro");
+        int numero = int.Parse(Console.ReadLine());
+        if (numero % 2 == 0){
+            Console.WriteLine($"o numero {numero} é par");
+            
+            
+            
+        }
+        else{
+           Console.WriteLine($"o numero {numero} não é par");
+            
+            
+            
+            
+            
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
