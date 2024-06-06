4379f027dd668e58b719d4809a768444504dc5f1
miguel5407 <161770352+miguel5407@users.noreply.github.com>
2024-06-06 15:05:23 -0300

    Update exercicio1.cs

---
+++ aula8/exercicio1.cs
@@ -0,0 +1,19 @@
+using System;
+class program
+{
+static int verPar()
+static void Main()
+{ 
+//criando o vetor
+int[] inteiros = new int[10];
+//usando o for
+for(int i = 0; i < inteiros.Length; i++)
+{
+Console.Write($"inteiro {i+1}:");
+inteiros[i] = int.Parse(Console.ReadLine());
+}
+{
+Console.WriteLine($"os numeros pares são: {inteirosi[i]}");
+}
+}
+}
