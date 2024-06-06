48a02a92586f495051f587b7deeb7bb96cd33dd8
miguel5407 <miguelantonioteixeirarosa@gmail.com>
2024-06-06 18:38:14 -0300

    Update exercicio1.cs

--- aula8/exercicio1.cs
+++ aula8/exercicio1.cs
@@ -1,19 +1,55 @@
 using System;
-class program
+
+class Program
 {
-static int verPar()
-static void Main()
-{ 
-//criando o vetor
-int[] inteiros = new int[10];
-//usando o for
-for(int i = 0; i < inteiros.Length; i++)
-{
-Console.Write($"inteiro {i+1}:");
-inteiros[i] = int.Parse(Console.ReadLine());
-}
-{
-Console.WriteLine($"os numeros pares são: {inteirosi[i]}");
-}
-}
+    static void Main(string[] args)
+    {
+        // Criando o vetor principal
+        int[] inteiros = new int[10];
+
+        // Usando o for para leitura dos números
+        Console.WriteLine("Digite 10 números inteiros:");
+        for (int i = 0; i < inteiros.Length; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            inteiros[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Criando os vetores para pares e ímpares
+        int[] par = new int[10];
+        int[] impar = new int[10];
+        int countPar = 0;
+        int countImpar = 0;
+
+        // Separando os números em pares e ímpares
+        for (int i = 0; i < inteiros.Length; i++)
+        {
+            if (inteiros[i] % 2 == 0)
+            {
+                par[countPar] = inteiros[i];
+                countPar++;
+            }
+            else
+            {
+                impar[countImpar] = inteiros[i];
+                countImpar++;
+            }
+        }
+
+        // Exibindo os vetores de números pares
+        Console.WriteLine("Vetor de números pares:");
+        for (int i = 0; i < countPar; i++)
+        {
+            Console.Write(par[i] + " ");
+        }
+        Console.WriteLine();
+
+        // Exibindo os vetores de números ímpares
+        Console.WriteLine("Vetor de números ímpares:");
+        for (int i = 0; i < countImpar; i++)
+        {
+            Console.Write(impar[i] + " ");
+        }
+        Console.WriteLine();
+    }
 }
