05062369de8524aa627275038a967ba6c8ee91e0
miguel5407 <miguelantonioteixeirarosa@gmail.com>
2024-06-16 19:44:57 -0300

    Create exercicio3.cs

---
+++ aula9/exercicio3.cs
@@ -0,0 +1,32 @@
+using System;
+class Program{
+    struct cadastro{
+    public string nome;
+    public string poder;
+    public int pontuacao;
+    }
+    static void CadastrarHeroi(){
+    Console.WriteLine("nome, poder e pontuação: ");
+    Console.WriteLine("herói 1: ");
+    a1.nome = Console.ReadLine();
+    a1.poder = Console.ReadLine();
+    a1.pontuacao = int.Parse(Console.ReadLine());
+    Console.WriteLine("herói 2: ");
+    a2.nome = Console.ReadLine();
+    a2.poder = Console.ReadLine();
+    a2.pontuacao = int.Parse(Console.ReadLine());
+    Console.WriteLine("herói 3: ");
+    a3.nome = Console.ReadLine();
+    a3.poder = Console.ReadLine();
+    a3.pontuacao = int.Parse(Console.ReadLine());
+    Console.WriteLine("herói 4: ");
+    a4.nome = Console.ReadLine();
+    a4.poder = Console.ReadLine();
+    a4.pontuacao = int.Parse(Console.ReadLine());
+    Console.WriteLine("herói 5: ");
+    a5.nome = Console.ReadLine();
+    a5.poder = Console.ReadLine();
+    a5.pontuacao = int.Parse(Console.ReadLine());
+    
+    }
+}
