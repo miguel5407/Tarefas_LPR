15aabb2d36f85d4a5405ead34546ed87c7d7680c
miguel5407 <miguelantonioteixeirarosa@gmail.com>
2024-07-04 18:46:02 -0300

    Update exercicio3.cs

--- aula10/exercicio3.cs
+++ aula10/exercicio3.cs
@@ -1,55 +1,59 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-
-class Program
-{
-    static void Main()
-    {
-        // 1. Criação do Dicionário
-        Dictionary<string, int> pessoas = new Dictionary<string, int>
+class Program{
+static void Main(){
+Dictionary <string,int> pessoas = new Dictionary <string,int>();
+Console.WriteLine("digite quantas pessoas deseja inserir");
+int quantPessoas = int.Parse(Console.ReadLine());
+for(int i = 0; i < quantPessoas; i++){
+Console.WriteLine($"digite o nome da pessoa {i + 1}");
+string nome = Console.ReadLine();
+Console.WriteLine($"digite a idade de {nome}");
+int idade = int.Parse(Console.ReadLine());
+pessoas[nome] = idade;
+}
+int somaIdades = 0;
+foreach(var pessoa in pessoas){
+somaIdades += pessoa.Value;
+}
+double media = (double) somaIdades / quantPessoas;
+Console.WriteLine("pessoas com idade acima da média: ");
+foreach(var pessoa in pessoas){
+if(pessoa.Value > media){
+Console.WriteLine(pessoa.Key);
+}
+}
+string acharVelho = null;
+string acharNovo = null;
+int idadeMaisVelho = 0;
+int idadeMaisNovo = 999;
+foreach(var pessoa in pessoas){
+if(pessoa.Value > idadeMaisVelho){
+idadeMaisVelho = pessoa.Value;
+acharVelho = pessoa.Key;
+}
+if(pessoa.Value < idadeMaisNovo){
+idadeMaisNovo = pessoa.Value;
+acharNovo = pessoa.Key;
+}
+}
+Console.WriteLine($"pessoa mais velha: {acharVelho} com {idadeMaisVelho} anos");
+Console.WriteLine($"pessoa mais nova: {acharNovo} com {idadeMaisNovo} anos");
+Console.WriteLine("digite a idade que deseja remover");
+int idadeRemove = int.Parse(Console.ReadLine());
+List <string> chavesRemove = new List <string>();
+foreach(var pessoa in pessoas){
+if(pessoa.Value == idadeRemove){
+chavesRemove.Add(pessoa.Key);
+}
+}
+foreach(var chave in chavesRemove){
+pessoas.Remove(chave);
+}
+Console.WriteLine("dicionario atualizado!");
+foreach (var pessoa in pessoas)
         {
-            {"Ana", 25},
-            {"Bruno", 30},
-            {"Carla", 22},
-            {"Diego", 35},
-            {"Eva", 28}
-        };
-
-        // 2. Cálculo da Média de Idades
-        double media = pessoas.Values.Average();
-
-        // 3. Encontrar e Imprimir Nomes com Idades Acima da Média
-        Console.WriteLine("Pessoas com idade acima da média:");
-        foreach (var pessoa in pessoas)
-        {
-            if (pessoa.Value > media)
-            {
-                Console.WriteLine(pessoa.Key);
-            }
+            Console.WriteLine($"{pessoa.Key}: {pessoa.Value} anos");
         }
-
-        // 4. Encontrar e Imprimir a Pessoa Mais Velha e a Mais Nova
-        var pessoaMaisVelha = pessoas.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
-        var pessoaMaisNova = pessoas.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
-
-        Console.WriteLine($"Pessoa mais velha: {pessoaMaisVelha}");
-        Console.WriteLine($"Pessoa mais nova: {pessoaMaisNova}");
-
-        // 5. Remover Pessoas com Idade Igual a Y e Imprimir Dicionário Atualizado
-        Console.WriteLine("Digite a idade que deseja remover:");
-        int idadeParaRemover = int.Parse(Console.ReadLine());
-
-        var chavesParaRemover = pessoas.Where(p => p.Value == idadeParaRemover).Select(p => p.Key).ToList();
-        foreach (var chave in chavesParaRemover)
-        {
-            pessoas.Remove(chave);
-        }
-
-        Console.WriteLine("Dicionário atualizado:");
-        foreach (var pessoa in pessoas)
-        {
-            Console.WriteLine($"{pessoa.Key}: {pessoa.Value}");
-        }
-    }
+}
 }
